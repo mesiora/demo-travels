eeda02bdcb464c5e545b6aa8d047a0e6781ba730
Medusiora <khom@outsourcify.net>
2024-03-31 16:57:30 +0700

    feat: add postcss plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,9 @@ export default defineNuxtConfig({
 
   postcss: {
     plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+      "postcss-nested": {},
+      "postcss-hexrgba": {},
+      "postcss-import": {},
     },
   },
 });
