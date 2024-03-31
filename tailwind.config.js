5b94c2fabe274dcbe2b1c426690c3baf19c80c99
Medusiora <khom@outsourcify.net>
2024-03-31 15:17:57 +0700

    fix: remove content option for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
-  ],
   theme: {
     container: {
       center: true,
