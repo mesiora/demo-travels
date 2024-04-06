59e628d3ae000918f7433ad3174898c6a5c11677
Medusiora <khom@outsourcify.net>
2024-04-07 05:32:16 +0700

    feat: api handing err example

---
+++ error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{ props.error?.statusCode }}</h2>
+    <button @click="handleError">Clear errors</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  error: {
+    statusCode: number;
+    statusMessage: string;
+    message?: string;
+    stack?: string;
+    data?: any;
+  };
+}>();
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
--- pages/posts.vue
+++ pages/posts.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="container py-20">
-    {{ collection }}
+    <h1 class="text-5xl font-bold sm:text-7xl tracking-tight">Resources</h1>
+
+    <sub>
+      API powered by
+      <a
+        href="https://jsonplaceholder.typicode.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >{JSON} Placeholder</a
+      >
+    </sub>
+
+    <section
+      class="my-8 p-4 text-[#050708] bg-[#f0f0f0] rounded-lg w-full text-sm whitespace-pre"
+    >
+      <div v-if="pending">Loading ...</div>
+
+      <code v-else class="overflow-x-auto max-h-[50vh] block">
+        {{ collection }}
+      </code>
+    </section>
   </div>
 </template>
 
@@ -19,16 +39,23 @@ const filters = ref({
 
 const {
   data: collection,
-  pending: loading,
+  pending,
   error,
-  refresh,
 } = await useLazyAsyncData(
-  "/api/posts",
+  "api.posts",
   () => postStore.collection(filters.value),
   {
     watch: [filters],
   },
 );
+
+watch(error, (e) => {
+  // TODO: Handle error or display a toast
+  if (!e) return;
+
+  console.error(e);
+  alert(e.message);
+});
 </script>
 
 <style lang="scss" scoped></style>
