79c0e87183f216fbc993393d83244ed3c41e7713
Medusiora <38559820+medusiora@users.noreply.github.com>
2024-04-07 19:14:56 +0700

    Update AppFooter.vue

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,7 +2,7 @@
   <footer class="bg-neutral-100 py-6">
     <div class="container">
       <p class="text-xs tracking-tighter text-gray-500">
-        &copy; 2024 medusiora. All rights reserved.
+        &copy; 2024 medusiora - MIT License
       </p>
     </div>
   </footer>
