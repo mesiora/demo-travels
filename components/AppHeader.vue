5a1747d3832ffcd79b320ce9c06d183abbd7a70b
Medusiora <khom@outsourcify.net>
2024-04-07 18:40:57 +0700

    feat: language switcher

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,17 +1,21 @@
 <template>
   <nav class="fixed top-0 left-0 bg-white z-50 h-20 w-full">
-    <div class="container flex items-center h-full justify-between">
+    <div class="container flex items-center h-full">
       <NuxtLink :to="localePath({ name: 'index' })">
         <img src="~/assets/images/logo-no-background.svg" alt="Logo" />
       </NuxtLink>
 
-      <a
-        href="https://github.com/medusiora/nuxt-starter-template"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        GitHub
-      </a>
+      <div class="flex items-center gap-4 ml-auto">
+        <LanguageSwitcher />
+
+        <a
+          href="https://github.com/medusiora/nuxt-starter-template"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </div>
     </div>
   </nav>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
     </h1>
 
     <p class="mt-6 text-lg tracking-tight">
-      Nuxt 3 frontend template for third-party API integrations.
+      {{ $t("quote") }}
     </p>
 
     <div class="text-center my-8">
@@ -16,7 +16,7 @@
           type="button"
           class="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
         >
-          Get Started
+          {{ $t("get-started") }}
         </button>
       </NuxtLink>
     </div>
